eadcc850b48d6e8939686d2aab4ccce1c921937e
Simonsen94 <soerenkristiansimonsen@gmail.com>
2023-01-08 15:05:46 +0100

    update logo in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "gatsby";
-import logo from "../img/logo.svg";
+//import logo from "../img/logo.svg";
+import smetextlogo from "../img/smetextlogo.png"
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -41,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
+              <img src={smetextlogo} alt="SME Analyst" style={{ width: "100px"}} />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -66,16 +67,13 @@ const Navbar = class extends React.Component {
                 About
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                Services
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
               <Link className="navbar-item" to="/contact">
                 Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
               </Link>
             </div>
           </div>
